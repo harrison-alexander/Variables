349849d6f9060595642f4ac8e92acd18cc031ad1
Harrison Alexander <59713571+harrison-alexander@users.noreply.github.com>
2023-09-12 09:48:49 -0500

    Update Variables.java
    Final piece
    #master coder

--- src/Variables.java
+++ src/Variables.java
@@ -1,7 +1,6 @@
 public class Variables
 {
-    public static void main ( String[] args )
-    {
+    public static void main ( String[] args ) {
         //define 1 variable of each of the
         //following data types
         //byte      short      int     long
@@ -13,34 +12,34 @@ public class Variables
         short shortOne = -32123;
         int intOne = 90877;
         long longOne = 999999999;
-        float floatOne = 38.5678;
+        float floatOne = 38.5678F;
         double doubleOne = 923.234;
-        char charOne = "A"; 
+        char charOne = 'A';
         boolean booleanOne = true;
         String StringOne = "hello world";
         System.out.println("/////////////////////////////////");
-        System.out.println("*Harrison Alexander            07/18/02*");
+        System.out.println("*Harrison Alexander     07/18/02*");
         System.out.println("*                               *");
         System.out.println("*        integer types          *");
         System.out.println("*                               *");
-        System.out.println("*8 bit - byteOne = "+byteOne+"\t\t*");
-        System.out.println("*16 bit - shortOne = "+shortOne+"\t\t*";
-        System.out.println("*32 bit - intOne = "+intOne+"\t\t*";
-        System.out.println("*64 bit - longOne = "+longOne+"\t\t*";
+        System.out.println("*8 bit - byteOne = " + byteOne + "\t\t    *");
+        System.out.println("*16 bit - shortOne = " + shortOne + "\t\t*");
+        System.out.println("*32 bit - intOne = " + intOne + "\t\t*");
+        System.out.println("*64 bit - longOne = " + longOne + "\t*");
         System.out.println("*                               *");
-        System.out.println("*        real types          *");
+        System.out.println("*          real types           *");
         System.out.println("*                               *");
-        System.out.println("*32 bit - floatOne = "+floatOne+"\t\t*";
-        System.out.println("*64 bit - doubleOne = "+doubleOne+"\t\t*";
+        System.out.println("*32 bit - floatOne = " + floatOne + "\t*");
+        System.out.println("*64 bit - doubleOne = " + doubleOne + "\t*");
         System.out.println("*                               *");
-        System.out.println("*        other integer types          *");
+        System.out.println("*      other integer types      *");
         System.out.println("*                               *");
-        System.out.println("*16 bit - charOne = "+charOne+"\t\t*");
+        System.out.println("*16 bit - charOne = " + charOne + "\t\t    *");
         System.out.println("*                               *");
-        System.out.println("*        other types          *");
+        System.out.println("*         other types           *");
         System.out.println("*                               *");
-        System.out.println("*booleanOne = "+booleanOne+"\t\t*"      );                              *");
-        System.out.println("*StringOne = "+StringOne+"\t\t)*";                              *");
-        System.out.println("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+        System.out.println("*booleanOne = " + booleanOne + "\t\t        *");
+        System.out.println("*StringOne = " + StringOne + "\t\t*");
+        System.out.println("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
     }
 }
