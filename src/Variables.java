0f2cda9772bed7d4812b44f5a31ba9375c6f5b48
Harrison Alexander <59713571+harrison-alexander@users.noreply.github.com>
2023-09-11 23:28:50 -0500

    Update Variables.java

--- src/Variables.java
+++ src/Variables.java
@@ -11,12 +11,22 @@ public class Variables
         //integer variables
         byte byteOne = 127;
         short shortOne = -32123;
-
+        int intOne = 90877;
+        long longOne = 999999999;
+        float floatOne = 38.5678;
+        double doubleOne = 923.234;
+        char charOne = "A"; 
+        boolean booleanOne = true;
+        String StringOne = "hello world";
         System.out.println("/////////////////////////////////");
-        System.out.println("*Some Person            07/18/02*");
+        System.out.println("*Harrison Alexander            07/18/02*");
         System.out.println("*                               *");
         System.out.println("*        integer types          *");
         System.out.println("*                               *");
         System.out.println("*8 bit - byteOne = "+byteOne+"\t\t*");
+        System.out.println("*16 bit - shortOne = "+shortOne+"\t\t*";
+        System.out.println("*32 bit - intOne = "+intOne+"\t\t*";
+        System.out.println("*64 bit - longOne = "+longOne+"\t\t*";
+
     }
 }
